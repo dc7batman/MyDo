57942133b96d709b6bd8c800a6303828a5972d85
Mohan-hike <mohan@hike.in>
2016-12-27 02:03:43 +0530

    Add base project

---
+++ MyDoApp/MyDo/MyDo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyDo
+//
+//  Created by Mohan on 25/12/16.
+//  Copyright © 2016 eventfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
