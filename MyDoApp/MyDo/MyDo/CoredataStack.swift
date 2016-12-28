40d8aaeee8d650098d703a24b28d0cba6275460b
Mohan-hike <mohan@hike.in>
2016-12-28 22:07:16 +0530

    Setup core data stack
    Create core data stack

--- MyDoApp/MyDo/MyDo/CoredataStack.swift
+++ MyDoApp/MyDo/MyDo/CoredataStack.swift
@@ -7,7 +7,55 @@
 //
 
 import UIKit
+import CoreData
 
 class CoredataStack: NSObject {
-
+    
+    var managedObjectModel : NSManagedObjectModel?
+    var storeCoordinator : NSPersistentStoreCoordinator?
+    var store : NSPersistentStore?
+    
+    override init() {
+        super.init()
+        setupManagedObjectModel()
+        setupStoreCoordinator()
+        setupStore()
+    }
+    
+    func setupManagedObjectModel() {
+        let modelurl = Bundle.main.url(forResource: "Model", withExtension: "momd")
+        managedObjectModel = NSManagedObjectModel.init(contentsOf: modelurl!)
+    }
+    
+    func setupStoreCoordinator() {
+        storeCoordinator = NSPersistentStoreCoordinator.init(managedObjectModel: managedObjectModel!)
+    }
+    
+    func setupStore() {
+        let options = [NSMigratePersistentStoresAutomaticallyOption: true,
+                       NSInferMappingModelAutomaticallyOption: true]
+        
+        let storeUrl : URL = databaseFilePath() as! URL
+        
+        do {
+            store = try storeCoordinator!.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: "ModelConfig", at: storeUrl, options: options)
+        }
+        catch {
+            assert(false, "Store creation failed")
+        }
+    }
+    
+    func databaseFilePath() -> NSURL? {
+        
+        let libraryUrl = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first
+        let fileUrl = libraryUrl?.appendingPathComponent("AppLibrary")
+        if !FileManager.default.fileExists(atPath: (fileUrl?.path)!) {
+            do {
+                try FileManager.default.createDirectory(at: fileUrl!, withIntermediateDirectories: false, attributes: nil)
+            } catch {
+                assert(false, "Creating database directory failed")
+            }
+        }
+        return fileUrl?.appendingPathComponent("itemsdatbase.sqlite") as NSURL?
+    }
 }
--- MyDoApp/MyDo/MyDo/ViewController.swift
+++ MyDoApp/MyDo/MyDo/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let _ = CoredataStack.init()
     }
 
     override func didReceiveMemoryWarning() {
