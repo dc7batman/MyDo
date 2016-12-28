7af919d90f307935626aa294497ecd641ac69413
Mohan-hike <mohan@hike.in>
2016-12-28 20:50:40 +0530

    Add coredata model

---
+++ MyDoApp/MyDo/MyDo/CoredataStack.swift
@@ -0,0 +1,13 @@
+//
+//  CoredataStack.swift
+//  MyDo
+//
+//  Created by Mohan on 28/12/16.
+//  Copyright © 2016 eventfy. All rights reserved.
+//
+
+import UIKit
+
+class CoredataStack: NSObject {
+
+}
