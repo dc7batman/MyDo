dcfead730cea29b0ea25a4f239ca3eb37434fdbc
Mohan-hike <mohan@hike.in>
2017-01-05 22:46:35 +0530

    Add arrow animation

--- MyDoApp/MyDo/MyDo/NavBarTitleView.swift
+++ MyDoApp/MyDo/MyDo/NavBarTitleView.swift
@@ -12,15 +12,19 @@ class NavBarTitleView: UIView {
     
     var titleLabel: UILabel?
     private var arrowImageView: UIImageView?
+    var isMenuShown = false
+    
 
     required override init(frame: CGRect) {
         super.init(frame: frame)
         addSubViews()
+        addTapGesture()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         addSubViews()
+        addTapGesture()
     }
     
     func addSubViews() {
@@ -33,14 +37,43 @@ class NavBarTitleView: UIView {
         self.addConstraint(NSLayoutConstraint.init(item: titleLabel!, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerX, multiplier: 1.0, constant: -16.0))
         self.addConstraint(NSLayoutConstraint.init(item: titleLabel!, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0.0))
         
-        arrowImageView = UIImageView.init(image: UIImage.init(named: "Triangle"))
+        arrowImageView = UIImageView.init(image: UIImage.init(named: "triangle"))
         arrowImageView?.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(arrowImageView!)
         
         self.addConstraint(NSLayoutConstraint.init(item: arrowImageView!, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0.0))
         self.addConstraint(NSLayoutConstraint.init(item: arrowImageView!, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: titleLabel!, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: 4.0))
-        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[view(==width)]", options: .init(rawValue: 0), metrics: ["width": 16], views: ["view": arrowImageView!]))
-        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view(==height)]", options: .init(rawValue: 0), metrics: ["height": 16], views: ["view": arrowImageView!]))
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[view(==width)]", options: .init(rawValue: 0), metrics: ["width": 12], views: ["view": arrowImageView!]))
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view(==height)]", options: .init(rawValue: 0), metrics: ["height": 12], views: ["view": arrowImageView!]))
     }
 
+    func addTapGesture() {
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(tapAction))
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+    func tapAction() {
+        rotateArrowIndicator()
+    }
+    
+    func rotateArrowIndicator() {
+        var fromValue: CGFloat = 0.0
+        var toValue = CGFloat(M_PI)
+        
+        if isMenuShown {
+            let temp: CGFloat = fromValue
+            fromValue = toValue;
+            toValue = temp
+        }
+        isMenuShown = !isMenuShown
+        
+        let rotationAnimation = CABasicAnimation.init(keyPath: "transform.rotation.z")
+        rotationAnimation.fromValue = fromValue
+        rotationAnimation.toValue = toValue
+        rotationAnimation.duration = 0.3
+        rotationAnimation.timingFunction = CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseOut)
+        rotationAnimation.isRemovedOnCompletion = false
+        rotationAnimation.fillMode = kCAFillModeForwards
+        arrowImageView?.layer.add(rotationAnimation, forKey: "rotationAnimation")
+    }
 }
